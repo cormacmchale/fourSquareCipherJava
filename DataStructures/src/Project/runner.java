6d63b4ca935b8a94b2d2524753e27f4e90f4a565
unknown <cormacmchale1@gmail.com>
2018-02-15 13:41:13 +0000

    more work done on project

--- DataStructures/src/Project/runner.java
+++ DataStructures/src/Project/runner.java
@@ -6,23 +6,47 @@ import java.util.Scanner;
 
 public class runner {
 
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		
+		//initial variable
 		String str;
-
+		fourSquarecipher table = new fourSquarecipher();
+		//scanners for keyboard and file
+		Scanner console = new Scanner (System.in);
+		Scanner encriptionText = new Scanner(new FileReader("res/PoblachtNaHEireann.txt"));
 		
-		final Scanner console = new Scanner (System.in);
+		//testing file reader
+		//char test = encriptionText.next().charAt(0);
+		//System.out.println(test); 
+		
 		System.out.println("Enter First Keyword for encription: ");
 		str = console.nextLine();
         ArrayList<Character> keyWord = new ArrayList<Character>();
+        
 		
 		char keyWordOne[] = str.toCharArray();
 		Arrays.sort(keyWordOne);
 		char compare = keyWordOne[0];
 		keyWord.add(compare);
-		//for (int i = 1; i<keyWordOne; )
-	}
-
-}
+		
+		for (int i = 1; i<keyWordOne.length; i++)
+		{
+			if(!(compare == keyWordOne[i]))
+			{
+				keyWord.add(keyWordOne[i]);
+			}
+			compare = keyWordOne[i];
+		}
+		
+		for (int i = 0; i<keyWord.size();i++)
+		{
+			System.out.println(keyWord.get(i));
+		}
+		
+		table.keyWords(keyWord);
+		table.display();
+		
+		encriptionText.close();
+	}//end main
+	
+}//end class
